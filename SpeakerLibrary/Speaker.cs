06e3d1f80086335df5059b999c675ae88df1a833
albertkozak <albert.kozak@live.com>
2020-03-31 13:54:03 -0700

    Add project files.

---
+++ SpeakerLibrary/Speaker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SpeakerLibrary
+{
+    class Speaker
+    {
+        public string SpeakerId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+        public string Specialization { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Employer { get; set; }
+
+    }
+}
